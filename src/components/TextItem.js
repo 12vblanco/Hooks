8d157581810ecf86fa8a1a4661080247c94f622f
Victor Blanco <12vblanco@gmail.com>
2022-09-18 11:27:36 +0100

    counter added

--- src/components/TextItem.js
+++ src/components/TextItem.js
@@ -1,16 +1,19 @@
+import { Input } from "antd";
 import React, { useState } from "react";
+import { BsFillBasket2Fill } from "react-icons/bs";
 import styled from "styled-components";
 import Card from "../UI/Card";
 
 const TextItem = (props) => {
-  // const onClickHandler = (toggleShow) => {
-  //   console.log("clicked");
-  //   {
-  //     !show;
-  //   }
-  // };
-
   const [show, toggleShow] = useState(true);
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    setCount(Math.max(count - 1, 0));
+  };
 
   return (
     <Card>
@@ -28,18 +31,40 @@ const TextItem = (props) => {
             <h2>{props.uom}</h2>
           </PriceDiv>
         )}
-
-        {!show && (
-          <PriceDiv>
-            <h2>something else</h2>
-            <h2>new</h2>
-          </PriceDiv>
-        )}
         <EditDiv>
           <Button onClick={() => toggleShow(!show)}>
-            {show ? "Show other" : "Show price"}
+            {show ? <BsFillBasket2Fill /> : "Show details"}
           </Button>
         </EditDiv>
+        {!show && (
+          <BasketDiv style={{ alignItems: "flex-start", marginLeft: "3%" }}>
+            <span
+              onClick={increaseCount}
+              style={{ fontSize: "65px", cursor: "pointer" }}
+            >
+              +
+            </span>
+            <Input
+              value={count}
+              size="large"
+              style={{
+                width: "50px",
+                height: "30px",
+                position: "relative",
+                top: "26px",
+                fontSize: "32px",
+                textAlign: "centre",
+              }}
+              placeholder="0"
+            />
+            <span
+              onClick={decreaseCount}
+              style={{ fontSize: "65px", cursor: "pointer" }}
+            >
+              -
+            </span>
+          </BasketDiv>
+        )}
       </DivFlex>
     </Card>
   );
@@ -47,10 +72,11 @@ const TextItem = (props) => {
 
 const DivFlex = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   width: 800px;
   height: 140px;
+  padding-left: 12px;
 `;
 
 const ImgDiv = styled.div`
@@ -97,6 +123,14 @@ const Button = styled.div`
   font-size: 22px;
   border-radius: 8px;
   cursor: pointer;
+`;
+
+const BasketDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex: 1;
 `;
 
 export default TextItem;
